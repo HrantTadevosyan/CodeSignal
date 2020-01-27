50b0fa3f804f905acad2d357b97e80bc7ed4bd0d
Hrant <hrant.tadevosyan@protonmail.com>
2020-01-27 11:43:27 +0400

    .

--- c++/arcade/almost_increasing_sequence.h
+++ c++/arcade/almost_increasing_sequence.h
@@ -1,24 +1,17 @@
+#include <iostream>
 #include <vector>
-
-
-int helper(std::vector<int> sequence, int start = 0) {
-    for (int i = i + 1; i < sequence.size(); i++)
-        if (sequence[i - 1] >= sequence[i])
-            return i;
-
-    return sequence.size();
-}
+#include <set>
 
 bool almostIncreasingSequence(std::vector<int> sequence) {
-    int count = helper(sequence);
-    if (count == sequence.size())
-        return true;
-
-    if (count == 1 || sequence[count - 2] < sequence[count])
-        return helper(sequence, count) == sequence.size();
-
-    if (sequence[count - 1] < sequence[count + 1]) 
-        return helper(sequence, count + 1) == sequence.size();
-
-    return false;
+    std::set<int> left;
+    std::set<int> right;
+    
+    for (int i = 0; i < sequence.size(); i++)
+        for (int j = i + 1; j < sequence.size(); j++) 
+            if (sequence[i] >= sequence[j]) {
+                left.insert(i);
+                right.insert(j);
+            }
+    
+    return left.size() <= 1 || right.size() <= 1;
 }
